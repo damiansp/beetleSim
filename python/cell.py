c20e421ce2af5239ecbbe7a1690e1f4058abd1f3
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-04-22 15:02:08 -0400

    add tree, beetle to cell

--- python/cell.py
+++ python/cell.py
@@ -8,6 +8,12 @@ class Cell:
     def __str__(self):
         return 'Cell at (%d, %d), alt: %.2f' % (self.x, self.y, self.alt)
 
+    def init_tree_population(self, Tree):
+        self.Tree = Tree
+
+    def init_beetle_population(self, Beetle):
+        
+
 
 # Test
 #c1 = Cell(0, 0, 33.78897)
