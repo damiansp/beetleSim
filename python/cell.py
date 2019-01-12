5e753f69e4c560c7059b9d608da06dab7b0e3231
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-01-12 18:31:28 -0500

    add lat/lon to Cell, fix Cell __str__, Cell.init_beetle_population()

--- python/cell.py
+++ python/cell.py
@@ -1,20 +1,24 @@
 class Cell:
-    def __init__(self, x, y, alt):
+    def __init__(self, x, y, lat, lon, alt):
+        '''x and y are simply int indices for the matrix'''
         assert x >= 0 and y >= 0, 'Cell coordinates must be positive integers'
         self.x = int(x)
         self.y = int(y)
+        self.lat = lat
+        self.lon = lon
         self.alt = alt
 
     def __str__(self):
-        return 'Cell at (%d, %d), alt: %.2f' % (self.x, self.y, self.alt)
+        return ('Cell at (%d, %d):\n  lat: %.4f\n  lon: %.4f\n  alt: %.2f'
+                % (self.x, self.y, self.lat, self.lon, self.alt))
 
     def init_tree_population(self, Tree):
         self.Tree = Tree
 
     def init_beetle_population(self, Beetle):
-        
+        self.Beetle = Beetle
 
 
 # Test
-#c1 = Cell(0, 0, 33.78897)
-#print(c1)
+c1 = Cell(0, 0, 33.78897, -127.2242, 57.9)
+print(c1)
